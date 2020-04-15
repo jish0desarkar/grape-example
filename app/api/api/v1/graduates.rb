09a09588376765a7adeece0b0f4c8b588ce43214
jish0desarkar <desarkarjishnu@gmail.com>
2020-04-15 18:30:44 +0530

    feat: POST PUT DELETE requests added

--- app/api/api/v1/graduates.rb
+++ app/api/api/v1/graduates.rb
@@ -1,23 +1,45 @@
 module Api
     module V1
       class Graduates < Grape::API
-        include Api::V1::Defaults
-
+        prefix :api
+        version "v1", using: :path
+        default_format :json
+        format :json
         resource :graduates do
           desc "Return all graduates"
           get "", root: :graduates do
             Graduate.all
           end
-
-          desc "Return a graduate"
+          get ":id", root: :graduates do
+            Graduate.where(id: params[:id]).first!
+          end
+          desc "create a new Graduate"
           params do
-            requires :id, type: String, desc: "ID of the
-              graduate"
+              requires :name, type: String
           end
-          get ":id", root: "graduate" do
-            Graduate.where(id: permitted_params[:id]).first!
+          post do
+            Graduate.create!({
+              name:params[:name],
+            })
           end
-        end
-      end
+
+          desc "delete an Graduate"
+          params do
+            requires :id, type: String 
+          end
+          delete ':id' do
+            Graduate.find(params[:id]).destroy!
+          end
+          desc "update an name of the graduate"
+            params do 
+              requires :id, type: String
+            end
+            put ':id' do
+              Graduate.find(params[:id]).update({
+                      name:params[:name]
+                     })
+            end
+         end
+       end
     end
 end
